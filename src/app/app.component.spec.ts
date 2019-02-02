e5f55aa89617766b4b1f216da1bf271fff69770b
bhtaylor41 <bhtaylor@email.sc.edu>
2019-02-02 17:13:31 -0500

    Cleared routing error for unit testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,12 +3,14 @@ import { RouterTestingModule } from "@angular/router/testing";
 
 import { AppComponent } from "./app.component";
 import { AuthService } from "./auth/auth.service";
+import { HttpClientModule } from "@angular/common/http";
+ // import { Router, RouterOutlet } from "@angular/router";
 
 
 describe("AppComponent", () => {
   it("should create", () => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
+      declarations: [ AppComponent ], imports: [ RouterTestingModule, HttpClientModule ]
     });
     const fixture = TestBed.createComponent(AppComponent);
     const component = fixture.componentInstance;
