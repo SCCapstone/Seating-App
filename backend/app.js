361873a4b725ad6d81f8ae26e693cf24f84e4d70
Eddie <ggordon@email.sc.edu>
2019-02-20 12:21:59 -0500

    HTTP Headers Unblocked lol

--- backend/app.js
+++ backend/app.js
@@ -30,18 +30,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/", express.static(path.join(__dirname, "angular")));
 
-//app.use((req, res, next) => {
-//  res.setHeader("Access-Control-Allow-Origin", "*");
-//  res.setHeader(
-//    "Access-Control-Allow-Headers",
-//    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//  );
-//  res.setHeader(
-//    "Access-Control-Allow-Methods",
-//    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-//  );
-//  next();
-//});
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 
 app.use("/api/user", userRoutes);
 app.use("/api/reservations", reservationsRoutes);
