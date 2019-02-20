c5c213707c2eeba0c007cfd0b75801cab758580f
Eddie <ggordon@email.sc.edu>
2019-02-19 20:34:17 -0500

    Changes for Meeting

--- src/app/main/dashboard/side-servers/side-servers.component.ts
+++ src/app/main/dashboard/side-servers/side-servers.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Subscription } from "rxjs";
+import { ServersService } from "../../manager/servers/servers.service";
+import { Server } from "../../manager/servers/server.model";
+import { AuthService } from "../../../auth/auth.service";
 
 @Component({
   selector: 'app-side-servers',
@@ -6,10 +11,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-servers.component.css']
 })
 export class SideServersComponent implements OnInit {
-
-  constructor() { }
+  editServerID = "none";
+  servers: Server[] = [];
+  isLoading = false;
+  totalServers = 0;
+  serversPerPage = 10;
+  currentPage = 1;
+  userIsAuthenticated = false;
+  userId: string;
+  private serversSub: Subscription;
+  private authStatusSub: Subscription;
+  constructor(
+    public serversService: ServersService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
+    this.isLoading = true;
+    this.serversService.getServers(
+      this.serversPerPage,
+      this.currentPage
+    );
+    this.userId = this.authService.getUserId();
+    this.serversSub = this.serversService
+      .getServerUpdateListener()
+      .subscribe(
+        (serverData: {
+          servers: Server[];
+        }) => {
+          this.isLoading = false;
+          this.servers = serverData.servers;
+        }
+      );
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
+        this.userId = this.authService.getUserId();
+      });
   }
 
 
--- src/app/main/dashboard/side-store/side-store.component.ts
+++ src/app/main/dashboard/side-store/side-store.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import "fabric";
+import { FloorplansService } from "../../manager/fp-builder/floorplan.service";
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Floorplan } from '../../manager/fp-builder/floorplan.model';
+
+declare let fabric;
 
 @Component({
   selector: 'app-side-store',
@@ -6,10 +14,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-store.component.css']
 })
 export class SideStoreComponent implements OnInit {
+  private canvas;
+  private rectTable;
+  private circleTable;
+  private textBox;
 
-  constructor() { }
+  floorplan: Floorplan;
+  floorplanList: Floorplan[] = [];
+  selected = "None";
+  private mode = "create";
+  private floorplanId: string;
+  private floorplansSub: Subscription;
+  private authStatusSub: Subscription;
+
+  totalFloorplans = 0;
+  isLoading = false;
+  userIsAuthenticated = false;
+  userId: string;
+
+  constructor(
+    public floorplansService: FloorplansService,
+    public route: ActivatedRoute,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
+    this.isLoading = true;
+    this.floorplansService.getFloorplans();
+    this.userId = this.authService.getUserId();
+    this.floorplansSub = this.floorplansService
+      .getFloorplanUpdateListener()
+      .subscribe(
+        (floorplanData: {
+          floorplans: Floorplan[];
+          floorplanCount: number;
+        }) => {
+          this.isLoading = false;
+          this.totalFloorplans = floorplanData.floorplanCount;
+          this.floorplanList = floorplanData.floorplans;
+        }
+      );
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
+        this.userId = this.authService.getUserId();
+      });
+
+/*     this.authStatusSub = this.authService
+    .getAuthStatusListener()
+    .subscribe(authStatus => {
+      this.isLoading = false;
+    }); */
+    this.canvas = new fabric.Canvas("canvas", {});
+    const canvasSpec  = document.getElementById("canvas-wrap");
+    this.canvas.setHeight(canvasSpec.clientHeight - 50);
+    this.canvas.setWidth(canvasSpec.clientWidth);
   }
 
+  loadCanvas(id: string) {
+    // Currently prompts user for name. **TODO
+    console.log("Loading Floorplan with ID: " + id);
+
+    this.floorplansService.getFloorplan(id).subscribe(floorplanData => {
+      this.floorplan = {
+        id: floorplanData._id,
+        name: floorplanData.name,
+        json: floorplanData.json,
+        creator: floorplanData.creator
+      };
+      this.selected = floorplanData.name;
+      this.canvas.loadFromJSON(this.floorplan.json);
+    });
+    // Redraws the canvas.
+    this.canvas.renderAll();
+  }
 }
