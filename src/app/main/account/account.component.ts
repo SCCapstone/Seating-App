6aef29ff68400b377576534e08a01bc55795a83b
Justin Gallman <jgallmanhand@gmail.com>
2019-02-24 22:37:41 -0500

    Changes to accounts

--- src/app/main/account/account.component.ts
+++ src/app/main/account/account.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Inject, OnInit, OnDestroy, Input } from "@angular/core";
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Subscription } from "rxjs";
+import { AuthService } from "src/app/auth/auth.service";
+
+import { AccountsService } from "./accounts.service";
+
 
 @Component({
   selector: "app-account",
@@ -7,8 +15,144 @@ import { Component, OnInit } from "@angular/core";
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  isLoading = false;
+  currentPage = 1;
+  accountsPerPage = 10;
+  userIsAuthenticated = false;
+  userId: string;
+  private authStatusSub: Subscription;
+  constructor(
+    public dialog: MatDialog,
+    public accountsService: AccountsService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.isLoading = true;
+    this.accountsService.getAccounts(
+      this.accountsPerPage,
+      this.currentPage
+    );
+    this.currentPage;
+    this.userId = this.authService.getUserId();
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
+        this.userId = this.authService.getUserId();
+      });
   }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
+  }
+
+ 
+  openEditAccount(id: string): void {
+    this.accountsService.setAccountToEdit(id);
+    const dialogRef = this.dialog.open(AccountEditComponent, {
+      width: '500px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+}
+
+@Component({
+  selector: 'app-account-edit',
+  templateUrl: 'account-edit.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountEditComponent implements OnInit, OnDestroy {
+  accountToEdit = "none";
+  enteredName = "";
+  isLoading = false;
+  form: FormGroup;
+  private mode = "edit";
+  private accountId: string;
+  private authStatusSub: Subscription;
+  private rpDisplayName: string;
+
+  currentPage = 1;
+
+  userIsAuthenticated = false;
+  userId: string;
+
+  constructor(
+    public dialogRef: MatDialogRef<AccountEditComponent>,
+    public accountsService: AccountsService,
+    public accountservice: AccountsService,
+    public route: ActivatedRoute,
+    public authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    /*
+    this.serverToEdit = this.serversService.getServerToEdit();
+    console.log(this.serverToEdit);
+    this.storesService.getStores(this.storesPerPage, this.currentPage);
+    this.userId = this.authService.getUserId();
+    this.storesSub = this.storesService
+      .getStoreUpdateListener()
+      .subscribe(
+        (storeData: {
+          stores: Store[];
+          storeCount: number;
+        }) => {
+          this.isLoading = false;
+        }
+      );
+      */
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe(authStatus => {
+        this.isLoading = false;
+      });
+    this.form = new FormGroup({
+      name: new FormControl(null, {
+        validators: [Validators.required]
+      })
+    });
+  }
+
+  onUpdateAccount() {
+    if (this.form.invalid) {
+      return;
+    }
+    this.accountId = this.accountToEdit;
+    this.isLoading = true;
+    this.rpDisplayName = "";
+      this.accountsService.updateAccount(
+        this.accountId,
+        this.rpDisplayName,
+        this.form.value.name
+    );
+    this.isLoading = false;
+    this.dialogRef.close();
+    this.form.reset();
+  }
+
+  onDelete() {
+    this.isLoading = true;
+    this.accountsService.deleteAccount(this.accountToEdit).subscribe(
+      () => {
+        this.isLoading = false;
+        this.dialogRef.close();
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
--- src/app/main/account/accounts.service.ts
+++ src/app/main/account/accounts.service.ts
@@ -4,11 +4,88 @@ import { Subject } from "rxjs";
 import { map } from "rxjs/operators";
 import { Router } from "@angular/router";
 
-//import { environment } from "../../../../environments/environment";
-//import { Store } from "./store.model";
+import { environment } from "../../../environments/environment";
 
-@Injectable({providedIn: 'root'})
+
+ const BACKEND_URL = environment.apiUrl + "/accounts/";
+
+@Injectable({ providedIn: "root" })
 export class AccountsService {
+  accountToUpdate = "none";
+  private accounts: Account[] = [];
+  private accountsUpdated = new Subject<{
+    accounts: Account[];
+    accountCount: number;
+  }>();
+  
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) {}
+
+  getAccounts(accountsPerPage: number, currentPage: number) {
+    const queryParams = `?pagesize=${accountsPerPage}&page=${currentPage}`;
+    this.http
+      .get<{ message: string; accounts: any; maxAccounts: number }>(
+        BACKEND_URL + queryParams
+      )
+      .pipe(
+        map(accountData => {
+          return {
+            accounts: accountData.accounts.map(account => {
+              return {
+                name: account.name,
+                store: account.store,
+                id: account._id,
+                creator: account.creator
+              };
+            }),
+            maxAccounts: accountData.maxAccounts
+          };
+        })
+      )
+      .subscribe(transformedAccountData => {
+        this.accounts = transformedAccountData.accounts;
+        this.accountsUpdated.next({
+          accounts: [...this.accounts],
+          accountCount: transformedAccountData.maxAccounts
+        });
+      });
+  }
+
+  getAccountUpdateListener() {
+    return this.accountsUpdated.asObservable();
+  }
+
+  getAccount(id: string) {
+    return this.http.get<{
+      _id: string;
+      name: string;
+      store: string;
+      creator: string;
+    }>(BACKEND_URL + id);
+  }
+
+
+
+  updateAccount(id: string, name: string, rpDisplayName: string) {
+    let accountData: Account;
+    accountData = {
+      id: id,
+      displayName: name,
+      rpDisplayName: rpDisplayName
+    };
+    this.http.put(BACKEND_URL + id, accountData).subscribe(response => {
+      this.router.navigate(["/main/manager"]);
+    });
+  }
+
+  deleteAccount(accountId: string) {
+    return this.http.delete(BACKEND_URL + accountId);
+  }
+
+  setAccountToEdit(id: string) {
+    this.accountToUpdate = id;
+  }
+  getAccountToEdit() {
+    return this.accountToUpdate;
+  }
 }
