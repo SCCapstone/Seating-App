fe999bb7ac5e5ce510562cb9b3fc6902541a8e5a
Eddie <ggordon@email.sc.edu>
2019-03-21 23:59:08 -0400

    Landing Page Cosmetic Fix (Mobile Friendly Now)

--- src/app/auth/landing/landing.component.ts
+++ src/app/auth/landing/landing.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./landing.component.css"]
 })
 export class LandingComponent implements OnInit {
+
   constructor() {}
 
   ngOnInit() {}
