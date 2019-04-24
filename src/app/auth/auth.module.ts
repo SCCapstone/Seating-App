0cfb33fa74987248c0cdcb130d4aebda1d7fce4b
bhtaylor41 <bhtaylor@email.sc.edu>
2019-04-23 20:31:10 -0400

    video for milestone 16 created and on splash page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,9 +7,18 @@ import { SignupComponent } from "./signup/signup.component";
 import { AngularMaterialModule } from "../angular-material.module";
 import { AuthRoutingModule } from "./auth-routing.module";
 import { LandingComponent } from "./landing/landing.component";
+// import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatVideoModule } from "mat-video";
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent, LandingComponent],
-  imports: [CommonModule, AngularMaterialModule, FormsModule, AuthRoutingModule]
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    FormsModule,
+    AuthRoutingModule,
+    // BrowserAnimationsModule,
+    MatVideoModule
+  ]
 })
 export class AuthModule {}
