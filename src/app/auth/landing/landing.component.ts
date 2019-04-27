5db424f256eb5a5cc4fa9c6f66bf82b4bfe73291
Eddie <ggordon@email.sc.edu>
2019-04-27 10:10:12 -0400

    Auth Data Cleared on Landing Page #119

--- src/app/auth/landing/landing.component.ts
+++ src/app/auth/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../auth.service";
 
 @Component({
   selector: "app-landing",
@@ -7,7 +8,11 @@ import { Component, OnInit } from "@angular/core";
 })
 export class LandingComponent implements OnInit {
 
-  constructor() {}
+  constructor(
+    private AuthService: AuthService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.AuthService.logout();
+  }
 }
