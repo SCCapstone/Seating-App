b7a1c3d21f251202536b99bdedd19572e64e7a13
bhtaylor41 <bhtaylor@email.sc.edu>
2019-04-29 09:33:34 -0400

    fix second unit test for main component #125

--- src/app/app.main.component.spec.ts
+++ src/app/app.main.component.spec.ts
@@ -1,11 +1,14 @@
-/* import { async, TestBed } from "@angular/core/testing";
+import { async, TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 
 import { AppComponent } from "./app.component";
 import { AuthService } from "./auth/auth.service";
 import { HttpClientModule } from "@angular/common/http";
-
+// import { MatDialogModule } from "@angular/common/http";
 import { MainComponent } from "./main/main.component";
+import { MatSidenavModule, MatDialogModule } from "@angular/material";
+import { SidenavComponent } from "./main/sidenav/sidenav.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 // import { StoresService } from "./stores.service";
 // import { Store } from "./store.model";
@@ -14,11 +17,13 @@ import { MainComponent } from "./main/main.component";
 describe("MainComponent", () => {
   it("should create", () => {
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ], imports: [ RouterTestingModule, HttpClientModule ]
+      declarations: [ MainComponent, AppComponent, SidenavComponent ],
+      imports: [ RouterTestingModule, HttpClientModule, MatSidenavModule, BrowserAnimationsModule, MatDialogModule ],
+      providers: []
     });
     const fixture = TestBed.createComponent(MainComponent);
     const component = fixture.componentInstance;
     expect(component).toBeDefined();
   });
 });
- */
+
